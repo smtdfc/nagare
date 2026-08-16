11615a00c326bc2a1e837b3e46151b9557806a46
smtdfc <admin.smtdfc@gmail.com>
2026-08-17 01:52:54 +0700

    chore: update

--- release.config.mjs
+++ release.config.mjs
@@ -3,4 +3,11 @@
  */
 export default {
   branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/github",
+    "@semantic-release/git",
+  ],
 };
