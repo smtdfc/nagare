b4f2f92a4de4088bd8419fec77b6dc5a99574f5f
smtdfc <admin.smtdfc@gmail.com>
2026-08-19 00:19:43 +0700

    feat(server): add plugin handler for websocket connections

--- server/ws/handlers/plugin.go
+++ server/ws/handlers/plugin.go
@@ -1,6 +1,34 @@
 package handlers
 
+import (
+	"github.com/smtdfc/nagare/core/global"
+	"github.com/smtdfc/nagare/shared/dto"
+	"github.com/smtdfc/nagare/shared/ws"
+)
+
 type PluginHandler struct {
+}
+
+func (h *PluginHandler) Register(i *ws.WsInstance, message *dto.WsMessage[any]) {
+	payload, err := ws.GetPayload[dto.PluginRegisterEvent](message)
+	if err != nil {
+		ws.SendMessage(i, dto.WS_PLUGIN_REGISTER_FAILED, dto.PluginRegisterFailedEvent{
+			Cause: "payload error: ",
+		})
+	}
+
+	if !global.GlobalPluginMgr.HasConnectCode(payload.Code) {
+		ws.SendMessage(i, dto.WS_PLUGIN_REGISTER_FAILED, dto.PluginRegisterFailedEvent{
+			Cause: "invalid connect code",
+		})
+		return
+	}
+
+	i.Auth = &dto.AuthPayload{
+		Target: "plugin",
+	}
+
+	ws.SendMessage(i, dto.WS_AUTH_SUCCESS, dto.PluginRegisterSuccessEvent{})
 }
 
 // @Injectable
