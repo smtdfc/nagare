05ac1f5c66ee658f02be3d51ef7b15206e800887
smtdfc <admin.smtdfc@gmail.com>
2026-08-19 00:17:13 +0700

    feat(shared): add DTOs for plugin registration events

--- shared/dto/ws_plugin.go
+++ shared/dto/ws_plugin.go
@@ -3,9 +3,19 @@ package dto
 const (
 	WS_PLUGIN_REGISTER         WsEvent = "WS_PLUGIN_REGISTER"
 	WS_PLUGIN_REGISTER_SUCCESS WsEvent = "WS_PLUGIN_REGISTER_SUCCESS"
-	WS_PLUGIN_REGISTER_FAIL    WsEvent = "WS_PLUGIN_REGISTER_FAIL"
+	WS_PLUGIN_REGISTER_FAILED  WsEvent = "WS_PLUGIN_REGISTER_FAIL"
 )
 
 // Event: WS_PLUGIN_REGISTER
-type WsPluginRegisterEvent struct {
+type PluginRegisterEvent struct {
+	Code string `json:"code" mapstructure:"code"`
+}
+
+// Event: WS_PLUGIN_REGISTER_SUCCESS
+type PluginRegisterSuccessEvent struct {
+}
+
+// Event: WS_PLUGIN_REGISTER_FAILED
+type PluginRegisterFailedEvent struct {
+	Cause string `json:"cause" mapstructure:"cause"`
 }
