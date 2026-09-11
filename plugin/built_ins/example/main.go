f40ae39015d65161c247725a1a3d28bacf14bd30
smtdfc <admin.smtdfc@gmail.com>
2026-09-12 02:03:29 +0700

    feat(plugin/example): enhance main function with metadata loading and handshake support

--- plugin/built_ins/example/main.go
+++ plugin/built_ins/example/main.go
@@ -1,8 +1,36 @@
 package main
 
-import "github.com/smtdfc/nagare/plugin/client"
+import (
+	"context"
+	_ "embed"
+	"fmt"
+
+	"github.com/smtdfc/nagare/plugin/client"
+)
+
+//go:embed metadata.json
+var metadata string
 
 func main() {
+	ctx := context.Background()
 	pluginClient := client.NewPlugin()
-	pluginClient.Start()
+	_, err := pluginClient.LoadMetadata(metadata)
+	if err != nil {
+		fmt.Println("Metadata error :", err)
+		return
+	}
+
+	err = pluginClient.Start(
+		ctx,
+		func() {
+			err := pluginClient.Handshake(ctx)
+			if err != nil {
+				fmt.Println("Handshake error :", err)
+				return
+			}
+		},
+	)
+	if err != nil {
+		fmt.Println("Plugin start error :", err)
+	}
 }
