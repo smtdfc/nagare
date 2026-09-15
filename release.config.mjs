679673898be44ead3499aa6a48f0ce95e34e2295
smtdfc <admin.smtdfc@gmail.com>
2026-09-15 13:34:04 +0700

    fix: correct formatting of GitHub plugin configuration in release.config.mjs

--- release.config.mjs
+++ release.config.mjs
@@ -9,20 +9,21 @@ export default {
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
-    "@semantic-release/github",
     [
       "@semantic-release/exec",
       {
         prepareCmd: "./build.sh",
       },
     ],
-    "@semantic-release/github",
-    {
-      assets: [
-        { path: "dist/cli/nagare", name: "nagare-${nextRelease.version}" },
-        { path: "dist/gateway/nagare-gateway", name: "nagare-gateway-${nextRelease.version}" }
-      ]
-    },
+    [
+      "@semantic-release/github",
+      {
+        assets: [
+          { path: "dist/cli/nagare", name: "nagare-${nextRelease.version}" },
+          { path: "dist/gateway/nagare-gateway", name: "nagare-gateway-${nextRelease.version}" }
+        ]
+      }
+    ],
     "@semantic-release/git",
   ],
 };
